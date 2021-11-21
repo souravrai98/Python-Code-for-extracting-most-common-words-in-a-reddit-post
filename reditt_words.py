6009a90b07fe5ecaa0c5e5b5ddb92791fa094e9a
souravrai98 <49655667+souravrai98@users.noreply.github.com>
2021-11-21 01:04:28 +0100

    Add files via upload
    Python code

---
+++ reditt_words.py
@@ -0,0 +1,50 @@
+import praw
+from nltk.corpus import stopwords
+import re
+from itertools import chain
+from collections import Counter
+
+reddit = praw.Reddit(
+    client_id="C71w7udlrNFUvaVEirRNNw",
+    client_secret="y0jjZYX1X4dyxXZz7khQ-5bGfoCTXg",
+    password="36chinatown",
+    user_agent="SOURAV RAI",
+    username="indianmonster",
+)
+
+submission = reddit.submission(url="https://www.reddit.com/r/anime/comments/m6odg8/your_top_10_anime_list/")
+submissionList = []
+submission.comments.replace_more(limit=None)
+for comment in submission.comments.list():
+    submissionList.append(comment)
+
+
+def word_extraction(sentence):    
+  ignore = stopwords.words('english')  
+  words = re.sub("[^\w]", " ",  sentence).split()    
+  cleaned_text = [w.lower() for w in words if w not in ignore]    
+  return cleaned_text
+
+
+def data_cleaning(comments):
+    l = []
+    for a in comments:
+        l.append(a.body)
+
+    lower_l=[]
+    for a in l:
+        lower_l.append(a.lower())
+
+    sans_virgules = [address.replace(',', ' ') for address in lower_l]
+    sans_virgules_1 = [address.replace('\n', ' ') for address in sans_virgules]
+
+
+    nostopwords = []
+    for l in sans_virgules_1:
+        nostopwords.append(word_extraction(l))
+        
+    return nostopwords
+
+cleaned = data_cleaning(submissionList)
+
+print(Counter(chain(*cleaned)).most_common(80))
